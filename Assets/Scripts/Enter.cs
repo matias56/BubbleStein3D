8a13bdbd514356b396b8230248fa42749f017e8a
unknown <matuge2010@gmail.com>
2025-01-26 17:38:03 -0300

    Gaia

--- Assets/Scripts/Enter.cs
+++ Assets/Scripts/Enter.cs
@@ -2,21 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Enter : MonoBehaviour
 {
+    public Image cred;
+    public float timer = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cred.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(cred.enabled == true)
         {
-            SceneManager.LoadScene("Game");
+            timer -= Time.deltaTime;
+
+            if(timer <= 0)
+            {
+                cred.enabled = false;
+                timer = 5;
+            }
         }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        cred.enabled = true;
     }
 }
