6f71233098548536eac29ac0af5fa9d015c709d1
unknown <matuge2010@gmail.com>
2025-01-26 13:30:07 -0300

    Deco

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class EnemyManager : MonoBehaviour
     public Enemy[] enemies;
 
     public GameObject gate;
+
+    public Text count;
+
+    public float en;
     
     // Start is called before the first frame update
     void Start()
@@ -25,6 +30,10 @@ public class EnemyManager : MonoBehaviour
         {
             gate.SetActive(true);
         }
+
+        en = enemies.Length;
+
+        count.text = "Dragons: " + enemies.Length;
     }
 
     
--- Assets/Scripts/EnemySpriteLook.cs
+++ Assets/Scripts/EnemySpriteLook.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class EnemySpriteLook : MonoBehaviour
 {
+    public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMove>().transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(player);
     }
 }
